3011bd0ae36a9e190e8d24add6c42e18a64f8b3f
raiderx154 <gabraider154@gmail.com>
2025-05-18 21:20:31 -0400

    front

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/vilfredos/Vídeos/logviewer/venv'
+set -gx VIRTUAL_ENV /home/gabriel/Documentos/logviewer/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
